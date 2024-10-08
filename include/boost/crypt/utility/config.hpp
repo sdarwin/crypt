b992e40c111d674f53edcf004068daeeba470fa6
Matt Borland <matt@mattborland.com>
2024-10-08 12:20:00 -0400

    Add table macro definitions

---
+++ include/boost/crypt/utility/config.hpp
@@ -0,0 +1,25 @@
+// Copyright 2024 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_CRYPT_DETAIL_CONFIG_HPP
+#define BOOST_CRYPT_DETAIL_CONFIG_HPP
+
+#ifdef __CUDACC__
+#  ifndef BOOST_CRYPT_ENABLE_CUDA
+#    define BOOST_CRYPT_ENABLE_CUDA
+#  endif
+#endif
+
+#if defined(__cpp_inline_variables) && __cpp_inline_variables >= 201606L
+#  define BOOST_CRYPT_CONSTEXPR_ARRAY inline constexpr
+#  define BOOST_CRYPT_DEVICE_ARRAY inline constexpr
+#elif defined(BOOST_CRYPT_ENABLE_CUDA)
+#  define BOOST_CYPRT_CONSTEXPR_ARRAY static constexpr
+#  define BOOST_CRYPT_DEVICE_ARRAY __constant__
+#else
+#  define BOOST_CRYPT_CONSTEXPR_ARRAY static constexpr
+#  define BOOST_CRYPT_DEVICE_ARRAY static constexpr
+#endif
+
+#endif //BOOST_CRYPT_DETAIL_CONFIG_HPP
