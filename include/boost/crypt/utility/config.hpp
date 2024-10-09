265ea28101b4e5e73dba475dab5819a9d9d37320
Matt Borland <matt@mattborland.com>
2024-10-09 11:55:33 -0400

    Add more GPU macros

--- include/boost/crypt/utility/config.hpp
+++ include/boost/crypt/utility/config.hpp
@@ -6,9 +6,34 @@
 #define BOOST_CRYPT_DETAIL_CONFIG_HPP
 
 #ifdef __CUDACC__
-#  ifndef BOOST_CRYPT_ENABLE_CUDA
-#    define BOOST_CRYPT_ENABLE_CUDA
+#  ifndef BOOST_CRYPT_HAS_CUDA
+#    define BOOST_CRYPT_HAS_CUDA
 #  endif
+#  define BOOST_CRYPT_GPU_ENABLED __host__ __device__
+#  define BOOST_CRYPT_GPU_HOST_ENABLED __host__
+#  define BOOST_CRYPT_GPU_DEVICE_ENABLED __device__
+#endif
+
+#ifdef __CUDACC_RTC__
+#  ifndef BOOST_CRYPT_HAS_CUDA
+#    define BOOST_CRYPT_HAS_CUDA
+#  endif
+#  define BOOST_CRYPT_HAS_NVRTC
+#  define BOOST_CRYPT_GPU_ENABLED __host__ __device__
+#  define BOOST_CRYPT_GPU_HOST_ENABLED __host__
+#  define BOOST_CRYPT_GPU_DEVICE_ENABLED __device__
+#endif
+
+#ifndef BOOST_CRYPT_GPU_ENABLED
+#  define BOOST_CRYPT_GPU_ENABLED
+#endif
+
+#ifndef BOOST_CRYPT_GPU_HOST_ENABLED
+#  define BOOST_CRYPT_GPU_HOST_ENABLED
+#endif
+
+#ifndef BOOST_CRYPT_GPU_DEVICE_ENABLED
+#  define BOOST_CRYPT_GPU_DEVICE_ENABLED
 #endif
 
 // ---- Constexpr arrays -----
