5269ece9ab13bb5aaa327e42167348f9518a8aeb
Matt Borland <matt@mattborland.com>
2024-10-11 16:45:00 -0400

    Unroll the hash loop

--- include/boost/crypt/hash/md5.hpp
+++ include/boost/crypt/hash/md5.hpp
@@ -23,36 +23,6 @@
 namespace boost {
 namespace crypt {
 
-namespace detail {
-
-static constexpr boost::crypt::array<boost::crypt::uint32_t, 64> S {
-        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-        5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
-        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
-};
-
-static constexpr boost::crypt::array<boost::crypt::uint32_t, 64> K {
-        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
-        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
-        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
-        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
-        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
-        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
-};
-
-} // namespace detail
-
 class md5_hasher
 {
 private:
@@ -235,53 +205,145 @@ BOOST_CRYPT_GPU_ENABLED constexpr auto md5_hasher::process_bytes(ForwardIter buf
     md5_update(buffer, byte_count);
 }
 
-// TODO(mborland): Replace the loop with the known statements
+// See: Applied Cryptography - Bruce Schneier
+// Section 18.5
+namespace md5_body_detail {
+
+BOOST_CRYPT_GPU_ENABLED constexpr auto F(boost::crypt::uint32_t x, boost::crypt::uint32_t y, boost::crypt::uint32_t z) noexcept
+{
+    return (x & y) | ((~x) & z);
+}
+
+BOOST_CRYPT_GPU_ENABLED constexpr auto G(boost::crypt::uint32_t x, boost::crypt::uint32_t y, boost::crypt::uint32_t z) noexcept
+{
+    return (x & z) | (y & (~z));
+}
+
+BOOST_CRYPT_GPU_ENABLED constexpr auto H(boost::crypt::uint32_t x, boost::crypt::uint32_t y, boost::crypt::uint32_t z) noexcept
+{
+    return x ^ y ^ z;
+}
+
+BOOST_CRYPT_GPU_ENABLED constexpr auto I(boost::crypt::uint32_t x, boost::crypt::uint32_t y, boost::crypt::uint32_t z) noexcept
+{
+    return y ^ (x | (~z));
+}
+
+BOOST_CRYPT_GPU_ENABLED constexpr auto FF(boost::crypt::uint32_t& a, boost::crypt::uint32_t b,  boost::crypt::uint32_t c,
+                                          boost::crypt::uint32_t d,  boost::crypt::uint32_t Mj, boost::crypt::uint32_t si,
+                                          boost::crypt::uint32_t ti) noexcept
+{
+    a = b + detail::rotl((a + F(b, c, d) + Mj + ti), si);
+}
+
+BOOST_CRYPT_GPU_ENABLED constexpr auto GG(boost::crypt::uint32_t& a, boost::crypt::uint32_t b,  boost::crypt::uint32_t c,
+                                          boost::crypt::uint32_t d,  boost::crypt::uint32_t Mj, boost::crypt::uint32_t si,
+                                          boost::crypt::uint32_t ti) noexcept
+{
+    a = b + detail::rotl((a + G(b, c, d) + Mj + ti), si);
+}
+
+BOOST_CRYPT_GPU_ENABLED constexpr auto HH(boost::crypt::uint32_t& a, boost::crypt::uint32_t b,  boost::crypt::uint32_t c,
+                                          boost::crypt::uint32_t d,  boost::crypt::uint32_t Mj, boost::crypt::uint32_t si,
+                                          boost::crypt::uint32_t ti) noexcept
+{
+    a = b + detail::rotl((a + H(b, c, d) + Mj + ti), si);
+}
+
+BOOST_CRYPT_GPU_ENABLED constexpr auto II(boost::crypt::uint32_t& a, boost::crypt::uint32_t b,  boost::crypt::uint32_t c,
+                                          boost::crypt::uint32_t d,  boost::crypt::uint32_t Mj, boost::crypt::uint32_t si,
+                                          boost::crypt::uint32_t ti) noexcept
+{
+    a = b + detail::rotl((a + I(b, c, d) + Mj + ti), si);
+}
+
+} // md5_body_detail
+
 BOOST_CRYPT_GPU_ENABLED constexpr auto md5_hasher::md5_body() noexcept -> void
 {
-    boost::crypt::uint32_t A {a0_};
-    boost::crypt::uint32_t B {b0_};
-    boost::crypt::uint32_t C {c0_};
-    boost::crypt::uint32_t D {d0_};
+    using namespace md5_body_detail;
 
-    for (boost::crypt::uint32_t i {}; i < 64U; ++i)
-    {
-        boost::crypt::uint32_t F {};
-        boost::crypt::uint32_t g {};
+    boost::crypt::uint32_t a {a0_};
+    boost::crypt::uint32_t b {b0_};
+    boost::crypt::uint32_t c {c0_};
+    boost::crypt::uint32_t d {d0_};
 
-        if (i <= 15U)
-        {
-            F = (B & C) | ((~B) & D);
-            g = i;
-        }
-        else if (i <= 31U)
-        {
-            F = (D & B) | ((~D) & C);
-            g = (5U * i + 1U) % 16U;
-        }
-        else if (i <= 47U)
-        {
-            F = B ^ C ^ D;
-            g = (3U * i + 5U) % 16U;
-        }
-        else
-        {
-            F = C ^ (B | (~D));
-            g = (7U * i) % 16U;
-        }
+    // Round 1
+    FF(a, b, c, d, blocks_[0],   7, 0xd76aa478);
+    FF(d, a, b, c, blocks_[1],  12, 0xe8c7b756);
+    FF(c, d, a, b, blocks_[2],  17, 0x242070db);
+    FF(b, c, d, a, blocks_[3],  22, 0xc1bdceee);
+    FF(a, b, c, d, blocks_[4],   7, 0xf57c0faf);
+    FF(d, a, b, c, blocks_[5],  12, 0x4787c62a);
+    FF(c, d, a, b, blocks_[6],  17, 0xa8304613);
+    FF(b, c, d, a, blocks_[7],  22, 0xfd469501);
+    FF(a, b, c, d, blocks_[8],   7, 0x698098d8);
+    FF(d, a, b, c, blocks_[9],  12, 0x8b44f7af);
+    FF(c, d, a, b, blocks_[10], 17, 0xffff5bb1);
+    FF(b, c, d, a, blocks_[11], 22, 0x895cd7be);
+    FF(a, b, c, d, blocks_[12],  7, 0x6b901122);
+    FF(d, a, b, c, blocks_[13], 12, 0xfd987193);
+    FF(c, d, a, b, blocks_[14], 17, 0xa679438e);
+    FF(b, c, d, a, blocks_[15], 22, 0x49b40821);
 
-        BOOST_CRYPT_ASSERT(i <= 63U);
+    // Round 2
+    GG(a, b, c, d, blocks_[1],   5, 0xf61e2562);
+    GG(d, a, b, c, blocks_[6],   9, 0xc040b340);
+    GG(c, d, a, b, blocks_[11], 14, 0x265e5a51);
+    GG(b, c, d, a, blocks_[0],  20, 0xe9b6c7aa);
+    GG(a, b, c, d, blocks_[5],   5, 0xd62f105d);
+    GG(d, a, b, c, blocks_[10],  9, 0x02441453);
+    GG(c, d, a, b, blocks_[15], 14, 0xd8a1e681);
+    GG(b, c, d, a, blocks_[4],  20, 0xe7d3fbc8);
+    GG(a, b, c, d, blocks_[9],   5, 0x21e1cde6);
+    GG(d, a, b, c, blocks_[14],  9, 0xc33707d6);
+    GG(c, d, a, b, blocks_[3],  14, 0xf4d50d87);
+    GG(b, c, d, a, blocks_[8],  20, 0x455a14ed);
+    GG(a, b, c, d, blocks_[13],  5, 0xa9e3e905);
+    GG(d, a, b, c, blocks_[2],   9, 0xfcefa3f8);
+    GG(c, d, a, b, blocks_[7],  14, 0x676f02d9);
+    GG(b, c, d, a, blocks_[12], 20, 0x8d2a4c8a);
 
-        F = F + A + detail::K[i] + blocks_[g];
-        A = D;
-        D = C;
-        C = B;
-        B = B + detail::rotl(F, detail::S[i]);
-    }
+    // Round 3
+    HH(a, b, c, d, blocks_[5],   4, 0xfffa3942);
+    HH(d, a, b, c, blocks_[8],  11, 0x8771f681);
+    HH(c, d, a, b, blocks_[11], 16, 0x6d9d6122);
+    HH(b, c, d, a, blocks_[14], 23, 0xfde5380c);
+    HH(a, b, c, d, blocks_[1],   4, 0xa4beea44);
+    HH(d, a, b, c, blocks_[4],  11, 0x4bdecfa9);
+    HH(c, d, a, b, blocks_[7],  16, 0xf6bb4b60);
+    HH(b, c, d, a, blocks_[10], 23, 0xbebfbc70);
+    HH(a, b, c, d, blocks_[13],  4, 0x289b7ec6);
+    HH(d, a, b, c, blocks_[0],  11, 0xeaa127fa);
+    HH(c, d, a, b, blocks_[3],  16, 0xd4ef3085);
+    HH(b, c, d, a, blocks_[6],  23, 0x04881d05);
+    HH(a, b, c, d, blocks_[9],   4, 0xd9d4d039);
+    HH(d, a, b, c, blocks_[12], 11, 0xe6db99e5);
+    HH(c, d, a, b, blocks_[15], 16, 0x1fa27cf8);
+    HH(b, c, d, a, blocks_[2],  23, 0xc4ac5665);
 
-    a0_ += A;
-    b0_ += B;
-    c0_ += C;
-    d0_ += D;
+    // Round 4
+    II(a, b, c, d, blocks_[0],   6, 0xf4292244);
+    II(d, a, b, c, blocks_[7],  10, 0x432aff97);
+    II(c, d, a, b, blocks_[14], 15, 0xab9423a7);
+    II(b, c, d, a, blocks_[5],  21, 0xfc93a039);
+    II(a, b, c, d, blocks_[12],  6, 0x655b59c3);
+    II(d, a, b, c, blocks_[3],  10, 0x8f0ccc92);
+    II(c, d, a, b, blocks_[10], 15, 0xffeff47d);
+    II(b, c, d, a, blocks_[1],  21, 0x85845dd1);
+    II(a, b, c, d, blocks_[8],   6, 0x6fa87e4f);
+    II(d, a, b, c, blocks_[15], 10, 0xfe2ce6e0);
+    II(c, d, a, b, blocks_[6],  15, 0xa3014314);
+    II(b, c, d, a, blocks_[13], 21, 0x4e0811a1);
+    II(a, b, c, d, blocks_[4],   6, 0xf7537e82);
+    II(d, a, b, c, blocks_[11], 10, 0xbd3af235);
+    II(c, d, a, b, blocks_[2],  15, 0x2ad7d2bb);
+    II(b, c, d, a, blocks_[9],  21, 0xeb86d391);
+
+    a0_ += a;
+    b0_ += b;
+    c0_ += c;
+    d0_ += d;
 }
 
 template <typename T>
