5969bca96879adab7b8b1b3aee1712625b135d16
Matt Borland <matt@mattborland.com>
2024-10-11 10:08:23 -0400

    Improve testing methodology

--- test/test_md5.cpp
+++ test/test_md5.cpp
@@ -8,52 +8,54 @@
 #include <boost/core/lightweight_test.hpp>
 #include <string>
 #include <array>
+#include <tuple>
+
+constexpr std::array<std::tuple<const char*, uint32_t, uint32_t, uint32_t, uint32_t>, 3> test_values =
+{
+    std::make_tuple("The quick brown fox jumps over the lazy dog", 0x9e107d9d, 0x372bb682, 0x6bd81d35, 0x42a419d6),
+    std::make_tuple("The quick brown fox jumps over the lazy dog.", 0xe4d909c2, 0x90d0fb1c, 0xa068ffad, 0xdf22cbd0),
+    std::make_tuple("", 0xd41d8cd9, 0x8f00b204, 0xe9800998, 0xecf8427e)
+};
 
 void basic_tests()
 {
-    const auto message_1_result {boost::crypt::md5("The quick brown fox jumps over the lazy dog")};
-    BOOST_TEST_EQ(message_1_result[0], 0x9e107d9d);
-    BOOST_TEST_EQ(message_1_result[1], 0x372bb682);
-    BOOST_TEST_EQ(message_1_result[2], 0x6bd81d35);
-    BOOST_TEST_EQ(message_1_result[3], 0x42a419d6);
-
-    const auto message_2_result {boost::crypt::md5("The quick brown fox jumps over the lazy dog.")};
-    BOOST_TEST_EQ(message_2_result[0], 0xe4d909c2);
-    BOOST_TEST_EQ(message_2_result[1], 0x90d0fb1c);
-    BOOST_TEST_EQ(message_2_result[2], 0xa068ffad);
-    BOOST_TEST_EQ(message_2_result[3], 0xdf22cbd0);
-
-    const auto message_3_result {boost::crypt::md5("")};
-    BOOST_TEST_EQ(message_3_result[0], 0xd41d8cd9);
-    BOOST_TEST_EQ(message_3_result[1], 0x8f00b204);
-    BOOST_TEST_EQ(message_3_result[2], 0xe9800998);
-    BOOST_TEST_EQ(message_3_result[3], 0xecf8427e);
+    for (const auto& test_value : test_values)
+    {
+        const auto message_result {boost::crypt::md5(std::get<0>(test_value))};
+        BOOST_TEST_EQ(message_result[0], std::get<1>(test_value));
+        BOOST_TEST_EQ(message_result[1], std::get<2>(test_value));
+        BOOST_TEST_EQ(message_result[2], std::get<3>(test_value));
+        BOOST_TEST_EQ(message_result[3], std::get<4>(test_value));
+    }
 }
 
 void string_test()
 {
-    std::string message_1 {"The quick brown fox jumps over the lazy dog"};
-    const auto message_1_result {boost::crypt::md5(message_1)};
-    BOOST_TEST_EQ(message_1_result[0], 0x9e107d9d);
-    BOOST_TEST_EQ(message_1_result[1], 0x372bb682);
-    BOOST_TEST_EQ(message_1_result[2], 0x6bd81d35);
-    BOOST_TEST_EQ(message_1_result[3], 0x42a419d6);
+    for (const auto& test_value : test_values)
+    {
+        const std::string string_message {std::get<0>(test_value)};
+        const auto message_result {boost::crypt::md5(string_message)};
+        BOOST_TEST_EQ(message_result[0], std::get<1>(test_value));
+        BOOST_TEST_EQ(message_result[1], std::get<2>(test_value));
+        BOOST_TEST_EQ(message_result[2], std::get<3>(test_value));
+        BOOST_TEST_EQ(message_result[3], std::get<4>(test_value));
+    }
+}
 
+void string_view_test()
+{
     #ifdef BOOST_CRYPT_HAS_STRING_VIEW
-    std::string_view view_1 {message_1};
-    const auto view_1_result {boost::crypt::md5(view_1)};
-    BOOST_TEST_EQ(view_1_result[0], 0x9e107d9d);
-    BOOST_TEST_EQ(view_1_result[1], 0x372bb682);
-    BOOST_TEST_EQ(view_1_result[2], 0x6bd81d35);
-    BOOST_TEST_EQ(view_1_result[3], 0x42a419d6);
+    for (const auto& test_value : test_values)
+    {
+        const std::string string_message {std::get<0>(test_value)};
+        const std::string_view string_view_message {string_message};
+        const auto message_result {boost::crypt::md5(string_view_message)};
+        BOOST_TEST_EQ(message_result[0], std::get<1>(test_value));
+        BOOST_TEST_EQ(message_result[1], std::get<2>(test_value));
+        BOOST_TEST_EQ(message_result[2], std::get<3>(test_value));
+        BOOST_TEST_EQ(message_result[3], std::get<4>(test_value));
+    }
     #endif
-
-    std::string message_2 {"The quick brown fox jumps over the lazy dog."};
-    const auto message_2_result {boost::crypt::md5(message_2.begin(), message_2.end())};
-    BOOST_TEST_EQ(message_2_result[0], 0xe4d909c2);
-    BOOST_TEST_EQ(message_2_result[1], 0x90d0fb1c);
-    BOOST_TEST_EQ(message_2_result[2], 0xa068ffad);
-    BOOST_TEST_EQ(message_2_result[3], 0xdf22cbd0);
 }
 
 void bad_input()
@@ -63,34 +65,35 @@ void bad_input()
     BOOST_TEST_EQ(null_message[1], 0x0);
     BOOST_TEST_EQ(null_message[2], 0x0);
     BOOST_TEST_EQ(null_message[3], 0x0);
-
-    std::string message_1 {"The quick brown fox jumps over the lazy dog"};
-    const auto message_1_result {boost::crypt::md5(message_1.begin(), message_1.begin())};
-    BOOST_TEST_EQ(message_1_result[0], 0x0);
-    BOOST_TEST_EQ(message_1_result[1], 0x0);
-    BOOST_TEST_EQ(message_1_result[2], 0x0);
-    BOOST_TEST_EQ(message_1_result[3], 0x0);
 }
 
 void test_class()
 {
     boost::crypt::detail::md5 hasher;
-    constexpr auto msg = "The quick brown fox jumps over the lazy dog";
-    hasher.process_bytes(msg, std::strlen(msg));
-    std::array<std::uint32_t, 4> message_1_result {};
-    hasher.get_digest(message_1_result.begin(), message_1_result.size());
 
-    BOOST_TEST_EQ(message_1_result[0], 0x9e107d9d);
-    BOOST_TEST_EQ(message_1_result[1], 0x372bb682);
-    BOOST_TEST_EQ(message_1_result[2], 0x6bd81d35);
-    BOOST_TEST_EQ(message_1_result[3], 0x42a419d6);
+    for (const auto& test_value : test_values)
+    {
+        const auto msg {std::get<0>(test_value)};
+        hasher.process_bytes(msg, std::strlen(msg));
+        std::array<std::uint32_t, 4> message_result {};
+        hasher.get_digest(message_result.begin(), message_result.size());
+
+        BOOST_TEST_EQ(message_result[0], std::get<1>(test_value));
+        BOOST_TEST_EQ(message_result[1], std::get<2>(test_value));
+        BOOST_TEST_EQ(message_result[2], std::get<3>(test_value));
+        BOOST_TEST_EQ(message_result[3], std::get<4>(test_value));
+
+        hasher.init();
+    }
 }
-
 
 int main()
 {
     basic_tests();
     string_test();
+    string_test();
+    string_view_test();
+
     bad_input();
 
     test_class();
