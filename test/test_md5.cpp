5887c9376ee7681c05f0a67a2739a9f920413b70
Matt Borland <matt@mattborland.com>
2024-10-11 14:04:24 -0400

    Switch test over to 8 bits instead of 32

--- test/test_md5.cpp
+++ test/test_md5.cpp
@@ -11,17 +11,26 @@
 #include <array>
 #include <tuple>
 
-constexpr std::array<std::tuple<const char*, uint32_t, uint32_t, uint32_t, uint32_t>, 9> test_values =
+constexpr std::array<std::tuple<const char*, std::array<uint16_t, 16>>, 9> test_values =
 {
-    std::make_tuple("The quick brown fox jumps over the lazy dog", 0x9e107d9d, 0x372bb682, 0x6bd81d35, 0x42a419d6),
-    std::make_tuple("The quick brown fox jumps over the lazy dog.", 0xe4d909c2, 0x90d0fb1c, 0xa068ffad, 0xdf22cbd0),
-    std::make_tuple("", 0xd41d8cd9, 0x8f00b204, 0xe9800998, 0xecf8427e),
-    std::make_tuple("ddcc8542894a27456bbeb43f51f38764c32f72ae", 0x3b340f00, 0x97312ec8, 0x2fa4da0d, 0x7da53002),
-    std::make_tuple("webmin1980", 0xb78aae35, 0x6709f8c3, 0x1118ea61, 0x3980954b),
-    std::make_tuple("$2y$10$EQAmcJw0cg.rt.6..SJ2bulFhDo0eWtuMhkfDMPGsNdap4xrOY61K", 0x40bbe664, 0x4efd9354, 0x078d8c70, 0xfb6c9f42),
-    std::make_tuple("pkirsanov", 0x8793ce04, 0xf0c5f1e8, 0xed1e0c78, 0xf249fe1b),
-    std::make_tuple("Eleanor", 0xd37e4317, 0x4905de70, 0xfbb5b038, 0xd7247f57),
-    std::make_tuple("The Whirlpool Galaxy is about 88% the size of the Milky Way, with a diameter of 76,900 light-years", 0xd5dfd7b4, 0x1235abc7, 0xa9a3205b, 0x6896f34d),
+    std::make_tuple("The quick brown fox jumps over the lazy dog",
+                    std::array<std::uint16_t, 16>{0x9e, 0x10, 0x7d, 0x9d, 0x37, 0x2b, 0xb6, 0x82, 0x6b, 0xd8, 0x1d, 0x35, 0x42, 0xa4, 0x19, 0xd6}),
+    std::make_tuple("The quick brown fox jumps over the lazy dog.",
+                    std::array<std::uint16_t, 16>{0xe4, 0xd9, 0x09, 0xc2, 0x90, 0xd0, 0xfb, 0x1c, 0xa0, 0x68, 0xff, 0xad, 0xdf, 0x22, 0xcb, 0xd0}),
+    std::make_tuple("",
+                    std::array<std::uint16_t, 16>{0xd4, 0x1d, 0x8c, 0xd9, 0x8f, 0x00, 0xb2, 0x04, 0xe9, 0x80, 0x09, 0x98, 0xec, 0xf8, 0x42, 0x7e}),
+    std::make_tuple("ddcc8542894a27456bbeb43f51f38764c32f72ae",
+                    std::array<std::uint16_t, 16>{0x3b, 0x34, 0x0f, 0x00, 0x97, 0x31, 0x2e, 0xc8, 0x2f, 0xa4, 0xda, 0x0d, 0x7d, 0xa5, 0x30, 0x02}),
+    std::make_tuple("webmin1980",
+                    std::array<std::uint16_t, 16>{0xb7, 0x8a, 0xae, 0x35, 0x67, 0x09, 0xf8, 0xc3, 0x11, 0x18, 0xea, 0x61, 0x39, 0x80, 0x95, 0x4b}),
+    std::make_tuple("$2y$10$EQAmcJw0cg.rt.6..SJ2bulFhDo0eWtuMhkfDMPGsNdap4xrOY61K",
+                    std::array<std::uint16_t, 16>{0x40, 0xbb, 0xe6, 0x64, 0x4e, 0xfd, 0x93, 0x54, 0x07, 0x8d, 0x8c, 0x70, 0xfb, 0x6c, 0x9f, 0x42}),
+    std::make_tuple("pkirsanov",
+                    std::array<std::uint16_t, 16>{0x87, 0x93, 0xce, 0x04, 0xf0, 0xc5, 0xf1, 0xe8, 0xed, 0x1e, 0x0c, 0x78, 0xf2, 0x49, 0xfe, 0x1b}),
+    std::make_tuple("Eleanor",
+                    std::array<std::uint16_t, 16>{0xd3, 0x7e, 0x43, 0x17, 0x49, 0x05, 0xde, 0x70, 0xfb, 0xb5, 0xb0, 0x38, 0xd7, 0x24, 0x7f, 0x57}),
+    std::make_tuple("The Whirlpool Galaxy is about 88% the size of the Milky Way, with a diameter of 76,900 light-years",
+                    std::array<std::uint16_t, 16>{0xd5, 0xdf, 0xd7, 0xb4, 0x12, 0x35, 0xab, 0xc7, 0xa9, 0xa3, 0x20, 0x5b, 0x68, 0x96, 0xf3, 0x4d}),
 };
 
 void basic_tests()
@@ -29,12 +38,13 @@ void basic_tests()
     for (const auto& test_value : test_values)
     {
         const auto message_result {boost::crypt::md5(std::get<0>(test_value))};
-        if (!(BOOST_TEST_EQ(message_result[0], std::get<1>(test_value)) &&
-              BOOST_TEST_EQ(message_result[1], std::get<2>(test_value)) &&
-              BOOST_TEST_EQ(message_result[2], std::get<3>(test_value)) &&
-              BOOST_TEST_EQ(message_result[3], std::get<4>(test_value))))
+        const auto valid_result {std::get<1>(test_value)};
+        for (std::size_t i {}; i < message_result.size(); ++i)
         {
-            std::cerr << "Failure with message: " << std::get<0>(test_value) << '\n';
+            if (!BOOST_TEST_EQ(message_result[i], valid_result[i]))
+            {
+                std::cerr << "Failure with: " << std::get<0>(test_value) << '\n';
+            }
         }
     }
 }
@@ -45,12 +55,13 @@ void string_test()
     {
         const std::string string_message {std::get<0>(test_value)};
         const auto message_result {boost::crypt::md5(string_message)};
-        if (!(BOOST_TEST_EQ(message_result[0], std::get<1>(test_value)) &&
-              BOOST_TEST_EQ(message_result[1], std::get<2>(test_value)) &&
-              BOOST_TEST_EQ(message_result[2], std::get<3>(test_value)) &&
-              BOOST_TEST_EQ(message_result[3], std::get<4>(test_value))))
+        const auto valid_result {std::get<1>(test_value)};
+        for (std::size_t i {}; i < message_result.size(); ++i)
         {
-            std::cerr << "Failure with message: " << std::get<0>(test_value) << '\n';
+            if (!BOOST_TEST_EQ(message_result[i], valid_result[i]))
+            {
+                std::cerr << "Failure with: " << std::get<0>(test_value) << '\n';
+            }
         }
     }
 }
@@ -63,12 +74,13 @@ void string_view_test()
         const std::string string_message {std::get<0>(test_value)};
         const std::string_view string_view_message {string_message};
         const auto message_result {boost::crypt::md5(string_view_message)};
-        if (!(BOOST_TEST_EQ(message_result[0], std::get<1>(test_value)) &&
-              BOOST_TEST_EQ(message_result[1], std::get<2>(test_value)) &&
-              BOOST_TEST_EQ(message_result[2], std::get<3>(test_value)) &&
-              BOOST_TEST_EQ(message_result[3], std::get<4>(test_value))))
+        const auto valid_result {std::get<1>(test_value)};
+        for (std::size_t i {}; i < message_result.size(); ++i)
         {
-            std::cerr << "Failure with message: " << std::get<0>(test_value) << '\n';
+            if (!BOOST_TEST_EQ(message_result[i], valid_result[i]))
+            {
+                std::cerr << "Failure with: " << std::get<0>(test_value) << '\n';
+            }
         }
     }
     #endif
@@ -94,12 +106,13 @@ void test_class()
         std::array<std::uint32_t, 4> message_result {};
         hasher.get_digest(message_result.begin(), message_result.size());
 
-        if (!(BOOST_TEST_EQ(message_result[0], std::get<1>(test_value)) &&
-              BOOST_TEST_EQ(message_result[1], std::get<2>(test_value)) &&
-              BOOST_TEST_EQ(message_result[2], std::get<3>(test_value)) &&
-              BOOST_TEST_EQ(message_result[3], std::get<4>(test_value))))
+        const auto valid_result {std::get<1>(test_value)};
+        for (std::size_t i {}; i < message_result.size(); ++i)
         {
-            std::cerr << "Failure with message: " << std::get<0>(test_value) << '\n';
+            if (!BOOST_TEST_EQ(message_result[i], valid_result[i]))
+            {
+                std::cerr << "Failure with: " << std::get<0>(test_value) << '\n';
+            }
         }
 
         hasher.init();
