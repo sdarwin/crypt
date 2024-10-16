8bf15a8b17142b69887154029aed1511c3d54fae
Matt Borland <matt@mattborland.com>
2024-10-16 17:02:14 -0400

    Add test set outline

---
+++ test/test_sha1.cpp
@@ -0,0 +1,12 @@
+// Copyright 2024 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/crypt/hash/sha1.hpp>
+#include <boost/uuid/detail/sha1.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+int main()
+{
+    return boost::report_errors();
+}
